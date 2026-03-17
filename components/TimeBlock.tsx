e561ac280d378d973bdb59327b6122aac7fb5c78
aynigaromer-stack <aynigaromer@gmail.com>
2026-03-17 12:24:59 +0800

    Update TimeBlock.tsx

--- components/TimeBlock.tsx
+++ components/TimeBlock.tsx
@@ -7,9 +7,9 @@ type TimeBlockProps = {
 export default function TimeBlock({ firstLook, secondLook, days }: TimeBlockProps) {
   return (
     <div className="time-block">
-      <div>First Look {firstLook}</div>
-      <div>Second Look {secondLook}</div>
-      <div className="time-days">✦ {days} days ✦</div>
+      <div>First Look &nbsp;{firstLook}</div>
+      <div>Second Look &nbsp;{secondLook}</div>
+      <div className="time-days">· {days} days ·</div>
     </div>
   );
 }
